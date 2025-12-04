4e12b24f18637a01f7a0e1e8fbf3f84d004efc5f
kseniya-firsova <ksusha.kabikova@gmail.com>
2025-12-04 12:15:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,18 @@
 const slides = document.querySelectorAll('.slide');
-const dots = document.querySelectorAll('.dot');
-let currentSlide = 0;
+let current = 0;
 
-function showSlide(index) {
-
-  if (index < 0 || index >= slides.length) return;
-
-  slides.forEach(slide => slide.classList.remove('active'));
-  dots.forEach(dot => dot.classList.remove('active'));
-
+function show(index) {
+  slides.forEach(s => s.classList.remove('active'));
   slides[index].classList.add('active');
-  dots[index].classList.add('active');
-  currentSlide = index;
+  current = index;
 }
 
+document.querySelector('.prev').onclick = () => {
+  current = (current === 0) ? slides.length - 1 : current - 1;
+  show(current);
+};
 
-document.querySelector('.prev').addEventListener('click', () => {
-  currentSlide = (currentSlide > 0) ? currentSlide - 1 : slides.length - 1;
-  showSlide(currentSlide);
-});
-
-document.querySelector('.next').addEventListener('click', () => {
-  currentSlide = (currentSlide < slides.length - 1) ? currentSlide + 1 : 0;
-  showSlide(currentSlide);
-});
-
-
-dots.forEach(dot => {
-  dot.addEventListener('click', () => {
-    const index = parseInt(dot.dataset.index);
-    showSlide(index);
-  });
-});
+document.querySelector('.next').onclick = () => {
+  current = (current === slides.length - 1) ? 0 : current + 1;
+  show(current);
+};
